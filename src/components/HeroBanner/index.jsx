32cbf84acb24745b05cec79b585f2069f0c9c739
Irina <irina.blumenfeld@netlify.com>
2023-04-07 09:44:36 -0400

    Add country flag

--- src/components/HeroBanner/index.jsx
+++ src/components/HeroBanner/index.jsx
@@ -37,8 +37,9 @@ const HeroBanner = (props) => {
                 {fields.title && <h1 data-sb-field-path=".title" className={styles.hero__title}>{fields.title}</h1>}
                 {/* {fields.subtitle && <p data-sb-field-path=".subtitle" className={styles.hero__subtitle}>{fields.subtitle}</p>} */}
 
-                {/* <img src="https://flagcdn.com/96x72/${countryCode?.toLowerCase()}.png" className="geolocation__flag" alt="${countryName}" /> */}
+               
                 <ul className={styles.geo}>
+                <img src={`https://flagcdn.com/96x72/${geo?.geo.country.code.toLowerCase()}.png`} className={styles.geolocation__flag} alt={`${geo?.geo.country.name}`} />
                     <li>Your city is: <span className={styles.details}>{geo?.geo.city}</span></li>
                     <li>Your country code: <span className={styles.details}>{geo?.geo.country.code}</span></li>
                     <li>Your country name: <span className={styles.details}>{geo?.geo.country.name}</span></li>
